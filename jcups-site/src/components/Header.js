0e26b874bae5628f7b87f3496c9348c2922fa43e
Dan Possehl <danielbpossehl@gmail.com>
2022-07-11 17:38:46 -0500

    first commit

---
+++ jcups-site/src/components/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 200px;
+  width: 100%;
+  background-color: pink;
+`;
+
+const Header = () => {
+  return (
+    <>
+      <Container></Container>
+    </>
+  );
+};
+
+export default Header;
---
+++ jcups-site/src/components/Main.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+
+const MainContainer = styled.div`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: column;
+`;
+
+const Main = () => {
+  return <></>;
+};
+
+export default Main;
